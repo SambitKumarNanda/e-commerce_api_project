caad035655a54186f3b11f2efba45effbfcf07b7
SambitKumarNanda <nanu2647542@gmail.com>
2023-06-23 10:16:52 +0530

    added app section of userprofile

--- userprofile/dashboard/urls.py
+++ userprofile/dashboard/urls.py
@@ -5,5 +5,4 @@ from .views import DasboardUserCartListAPIView, DashboardUserWishListAPIView
 urlpatterns = [
     path("users-cart/", DasboardUserCartListAPIView.as_view(), name="DasboardUserCartListAPIView"),
     path("users-wishlist/", DashboardUserWishListAPIView.as_view(), name="DashboardUserWishListAPIView"),
-
 ]
