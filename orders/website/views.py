8334bcce9db095d9ce0c4c7fb1d7b6bfe9602f40
SambitKumarNanda <nanu2647542@gmail.com>
2023-06-23 13:08:22 +0530

    order website order from cart added

--- orders/website/views.py
+++ orders/website/views.py
@@ -1,4 +1,5 @@
-from .serializers import WebsiteOrderModelCreateSerializer, WebsiteOrderModelListSerializer
+from .serializers import WebsiteOrderModelCreateSerializer, WebsiteOrderModelListSerializer, \
+    WebsiteOrderFromCartModelSerializer
 from ..models import OrderModel
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
@@ -10,9 +11,23 @@ class WebsiteOrderModelCreateAPIView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request):
-        serializer = WebsiteOrderModelCreateSerializer(data=request.data, context={"request" : request})
+        serializer = WebsiteOrderModelCreateSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
             serializer.save()
-            return Response({"message" : "Order created"}, status=status.HTTP_200_OK)
+            return Response({"message": "Order created"}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class WebsiteOrderModelFromCartCreateAPIView(generics.CreateAPIView):
+    queryset = OrderModel.objects.all()
+    serializer_class = WebsiteOrderFromCartModelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request):
+        serializer = WebsiteOrderFromCartModelSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Order created"}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
